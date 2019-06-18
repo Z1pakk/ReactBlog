c5213973f125413a9371118094bc866de1905fd0
Vlad Shusmkiy <wladik420@gmail.com>
2019-06-18 21:32:24 +0300

    add eslint and prettier

--- ReactBlog/ReactBlog/ClientApp/src/App.js
+++ ReactBlog/ReactBlog/ClientApp/src/App.js
@@ -1,14 +1,6 @@
-import React from 'react';
-import { Route } from 'react-router';
-import Layout from './components/Layout';
-import Home from './components/Home';
-import Counter from './components/Counter';
-import FetchData from './components/FetchData';
+import React from "react";
 
-export default () => (
-  <Layout>
-    <Route exact path='/' component={Home} />
-    <Route path='/counter' component={Counter} />
-    <Route path='/fetch-data/:startDateIndex?' component={FetchData} />
-  </Layout>
-);
+let a;
+console.log("test");
+const App = () => <div>Hello World</div>;
+export default App;
