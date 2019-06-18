014ece3704f4edad41b2b1794b16869416b8dc05
Vlad Shusmkiy <wladik420@gmail.com>
2019-06-18 23:02:30 +0300

    Add LoginForm

---
+++ ReactBlog/ReactBlog/ClientApp/src/components/forms/LoginForm.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+export class LoginForm extends Component {
+  state = {
+    data: {},
+    loading: false,
+    errors: {}
+  };
+
+  render() {
+    return <div></div>;
+  }
+}
+
+export default LoginForm;
--- ReactBlog/ReactBlog/ClientApp/src/components/pages/LoginPage.js
+++ ReactBlog/ReactBlog/ClientApp/src/components/pages/LoginPage.js
@@ -1,9 +1,12 @@
 import React from "react";
+import LoginForm from "../forms/LoginForm";
 
 const LoginPage = () => {
   return (
     <div>
       <h1>Login Page</h1>
+
+      <LoginForm />
     </div>
   );
 };
